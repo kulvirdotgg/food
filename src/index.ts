e26760751745be55a4920a5b588665b552eb3063
Kulvir Singh <kulvirs734@gmail.com>
2026-04-19 17:44:57 +0530

    feat: init of HTML and tw

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!")
+import index from "@/templates/index.html"
+
+const server = Bun.serve({
+    port: 3000,
+    idleTimeout: 60,
+    routes: {
+        "/": index, // serves the home page
+        "/health": new Response("ok"),
+    },
+    fetch() {
+        return new Response("Not found", {
+            status: 404,
+            headers: { "content-type": "text/plain; charset=utf-8" },
+        })
+    },
+})
+
+console.info(`Food app running at http://localhost:${server.port}`)
