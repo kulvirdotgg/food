e13c756690a56131cd2e697973d3bd1a4efdc588
Kulvir Singh <kulvirs734@gmail.com>
2026-05-05 14:05:27 +0530

    feat: formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import clerk from '@clerk/astro'
-import tailwindcss from "@tailwindcss/vite";
-import vercel from '@astrojs/vercel';
-
+import { defineConfig } from "astro/config"
+import clerk from "@clerk/astro"
+import tailwindcss from "@tailwindcss/vite"
+import vercel from "@astrojs/vercel"
 
 // https://astro.build/config
 export default defineConfig({
     adapter: vercel(),
     integrations: [clerk()],
-    output: 'server',
+    output: "server",
     vite: {
         plugins: [tailwindcss()],
     },
-});
+})
