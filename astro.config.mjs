7e6c3bf68e2fc838d3c0928bf7d6a6565ef6df57
Kulvir Singh <kulvirs734@gmail.com>
2026-05-05 13:56:19 +0530

    feat: added clerk auth lmaoo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import clerk from '@clerk/astro'
 import tailwindcss from "@tailwindcss/vite";
+import vercel from '@astrojs/vercel';
 
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
     adapter: vercel(),
     integrations: [clerk()],
+    output: 'server',
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
